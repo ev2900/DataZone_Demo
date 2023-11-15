88f2804840bcc9848808bfe3de6e01617f9e242b
Christopher Sharkey <ev2900@gmail.com>
2023-11-14 21:21:47 -0500

    Update 2_dataZone_configuration.py

--- 2_dataZone_configuration.py
+++ 2_dataZone_configuration.py
@@ -36,8 +36,6 @@ try:
 
     producer_compound_libraries_domain_id = r['domainId']
     
-    time.sleep(120)
-
 except Exception as e:
     print(e)
 
@@ -52,107 +50,7 @@ try:
 
     consumer_high_throughput_screening_domain_id = r['domainId']
     
-    time.sleep(120)
-
 except Exception as e:
     print(e)
 
     consumer_high_throughput_screening_domain_id = str(e).split()[-1]
-
-#
-# 3. Create an enviorment for each project
-#
-
-# Get the ID of each enviorment profile
-enviorment_profile_dict = {}
-
-r = dzc.list_environment_profiles(domainIdentifier = datazone_domain_id)
-
-for envioronment_profile in r['items']:
-    
-    enviorment_profile_dict[envioronment_profile['name']] = envioronment_profile['id']
-
-# Create the enviorments 
-
-# Producer - Compound Libraries
-try:
-    r = dzc.create_environment(
-        domainIdentifier = datazone_domain_id,
-        environmentProfileIdentifier = enviorment_profile_dict['DataLakeProfile'],
-        name = 'Data Lake Environment',
-        projectIdentifier = producer_compound_libraries_domain_id
-    )
-    
-    producer_compound_libraries_data_lake_environment_id = r['id']
-    
-    time.sleep(120)
-    
-except Exception as e:
-    print(e)
-    
-    producer_compound_libraries_data_lake_environment_id = ''
-
-# Consumer - High-Throughput Screening
-try:
-    r = dzc.create_environment(
-        domainIdentifier = datazone_domain_id,
-        environmentProfileIdentifier = enviorment_profile_dict['DataLakeProfile'],
-        name = 'Data Lake Environment',
-        projectIdentifier = consumer_high_throughput_screening_domain_id
-    )
-
-except Exception as e:
-    print(e)
-    
-#
-# 4. Create a data source for Glue databases
-#
-
-# Producer - Compound Libraries
-try:
-    r = dzc.create_data_source(
-        name = 'Glue Data Catalog Compound Libraries',
-        domainIdentifier = datazone_domain_id,
-        environmentIdentifier = producer_compound_libraries_data_lake_environment_id,
-        projectIdentifier = producer_compound_libraries_domain_id,
-        recommendation = {
-            'enableBusinessNameGeneration': True
-        },
-        type = 'glue',
-        configuration = {
-            'glueRunConfiguration' : {
-            'relationalFilterConfigurations': [
-                {
-                    'databaseName': 'compound_libraries',
-                    'filterExpressions': [
-                    {
-                        'expression': '*',
-                        'type': 'INCLUDE'
-                    }
-                    ]
-                }
-            ]
-            }
-        }
-    )
-    
-    producer_compound_libraries_data_source_id = r['id']
-    
-    time.sleep(120)
-    
-except Exception as e:
-    print(e)
-    
-    producer_compound_libraries_data_source_id = ''
-#
-# 5. Run the data source
-#
-
-try:
-    r = dzc.start_data_source_run(
-        dataSourceIdentifier = producer_compound_libraries_data_source_id,
-        domainIdentifier = datazone_domain_id
-    )
-
-except Exception as e:
-    print(e)
